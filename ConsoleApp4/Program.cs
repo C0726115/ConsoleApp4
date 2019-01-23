aa8ff99a59869060ebe8f3c7b627e05bc044c4dc
C0726115 <C0726115@mylambton.ca>
2019-01-23 11:57:37 -0500

    hii

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace Week3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            int i = 0;
+            while ( i<5 )
+            {
+               
+               
+                sum += i;
+                i++;
+
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            //C0726115RobinBhatia
         }
     }
 }
